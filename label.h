55cd4882fd762c247f569945c329b09e2a94de73
Geeho <786689512@qq.com>
2015-08-14 08:40:06 +0800

    Update label.h

--- label.h
+++ label.h
@@ -1,11 +1,13 @@
 #ifndef LABEL_H
 #define LABEL_H
 #include <QMouseEvent>
+#include <QEvent>
 class Label:public QWidget
 {
   Q_OBJECT
 protected:
   void mousePressEvent(QMouseEvent *event);
+  bool event(QEvent *e);
 }
 
 #endif
